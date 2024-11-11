47c95494f8cc0b55fad40a205b96091476b85d5a
Tiago Martins <tiagomartinscc@hotmail.com>
2024-11-11 07:23:18 -0300

    create user route

---
+++ src/lib/prisma.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client'
+import { env } from 'src/env'
+
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === 'dev'
+    ? ['query']
+    : [],
+})
