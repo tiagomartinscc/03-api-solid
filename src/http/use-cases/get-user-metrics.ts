29e3a69c26f7515b679415c63f9508b443fc82d1
Tiago Martins <tiagomartinscc@hotmail.com>
2024-11-16 14:02:12 -0300

    get user metrics use case

---
+++ src/http/use-cases/get-user-metrics.ts
@@ -0,0 +1,28 @@
+import { CheckInsRepository } from '../../repositories/check-ins-repository'
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+  ) {}
+
+  async execute({
+    userId,
+  }: GetUserMetricsUseCaseRequest):
+    Promise<GetUserMetricsUseCaseResponse> {
+    const checkInsCount = await this.checkInsRepository.countByByUserId(
+      userId,
+    )
+
+    return {
+      checkInsCount,
+    }
+  }
+}
--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,5 +3,6 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date:Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByByUserId(userId: string): Promise<number>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
