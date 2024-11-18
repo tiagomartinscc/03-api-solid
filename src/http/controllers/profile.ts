82217e50a2199d6a01a22a1f36e048b696848b31
Tiago Martins <tiagomartinscc@hotmail.com>
2024-11-17 22:56:08 -0300

    create middleware to validate JWT

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,13 +1,21 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeGetUserProfileUseCase }
+  from 'src/use-cases/factories/make-get-user-profile-use-case'
 
 export async function profile(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
   try {
-    await request.jwtVerify()
+    const getUserProfile = makeGetUserProfileUseCase()
+    const { user } = await getUserProfile.execute({ userId: request.user.sub })
 
-    return reply.status(200).send()
+    return reply.status(200).send({
+      user: {
+        ...user,
+        password_hash: undefined,
+      },
+    })
   } catch (error) {
 
   }
