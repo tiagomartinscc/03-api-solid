efc21276930d1a3e5cf16327bc1736d28594586f
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-07 14:42:56 -0300

    refresh-token

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyJwt } from '../../middlewares/verify-jwt'
 import { search } from './search'
 import { create } from './create'
 import { nearby } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
   /** Authenticate */
-  app.addHook('onRequest', verifyJWT)
+  app.addHook('onRequest', verifyJwt)
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
   app.post('/gyms', create)
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,12 +2,14 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
-import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyJwt } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   /** Authenticate */
-  app.get('/me', { onRequest: verifyJWT }, profile)
+  app.get('/me', { onRequest: verifyJwt }, profile)
 }
